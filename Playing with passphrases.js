87643a483bb43a0fd764bf8401eb3b95f02c1797
lizhicheng <lizhicheng@3tmall.com>
2019-05-23 14:56:22 +0800

    Playing with passphrases 根据一些规则加密字符串

---
+++ Playing with passphrases.js
@@ -0,0 +1,13 @@
+function playPass(s, n) {
+    return [...s].map((char, index) => {
+        if (/[A-Z]/.test(char)) {
+            const shiftChar = String.fromCharCode(65 + (char.charCodeAt() - 65 + n) % 26);
+            return index % 2 ? shiftChar.toLowerCase() : shiftChar;
+        } else if (/[0-9]/.test(char)) {
+            return 9 - char
+        } else {
+            return char;
+        }
+    }).reverse().join('')
+
+}
